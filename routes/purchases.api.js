fe5566a332e24cb6b32302be9453e406e666d777
nicewjdqls <nicewjdqls@naver.com>
2024-09-12 16:18:42 +0900

    Update purchases.api.js

--- routes/purchases.api.js
+++ routes/purchases.api.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const purchasesController = require('../controller/purchases.controller');
 
 // 방 예약 요청
-router.post('/', purchasesController.reserveRoom);
-
-// 사용자 구매 내역 조회 (옵션)
-router.get('/user', purchasesController.getUserPurchases);
-
-// 방 예약 가능 여부 확인 (옵션)
-router.get('/roomcheck', purchasesController.checkRoomAvailability);
+router.post('/', purchasesController.reservation);
 
 module.exports = router;
